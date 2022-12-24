91f8d90692a59dda8f0e74571e46b65ddab7c16e
shivambadoni19992 <shivambadoni.39@gmail.com>
2022-12-24 12:47:09 +0530

    msg

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -8,8 +8,12 @@ import lombok.Setter;
 
 @Data
 public class DeliveryPartner {
+    
     private String id;
     private int numberOfOrders;
+    public DeliveryPartner(String partnerId) {
+        this.id=partnerId;
+    }
 
     
 
